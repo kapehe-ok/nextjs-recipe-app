5674aceb9fa505045f95d0608dcf2090b9b73016
Knut Melvær <knut.melvaer@gmail.com>
2021-04-27 17:38:06 +0200

    fix smol bug

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,16 +1,16 @@
-import {sanityClient} from "../../lib/client";
+import { sanityClient } from '../../lib/client'
 
 export default function OneRecipe({ recipe }) {
   return (
-    <>
+    <React.Fragment>
       <h1>This is my first recipe! And it's yummy!</h1>
       <h2>{recipe?.name}</h2>
       {/* <img src={recipe.image} /> */}
 
       {/* probably need to map over ingredients since it will be an array */}
       {/* <div>{recipe.ingredients}</div> */}
-    </>
-  );
+    </React.Fragment>
+  )
 }
 
 // STEP 1: tell next.js how many recipe pages it needs to make
@@ -19,10 +19,10 @@ export async function getStaticPaths() {
   const paths = await sanityClient.fetch(
     `*[_type == "recipe" && defined(slug.current)]{
       "params": {
-        "slug": slug.current`
+        "slug": slug.current
       }
-    }
-  );
+    }`
+  )
 
   return {
     paths,
@@ -37,8 +37,8 @@ export async function getStaticPaths() {
     // goes to check sanity for the content every page load
     // if content is changed/new, then next.js gives user the new version
     // if content is the same, next.js gives user the cached static HTML version
-    fallback: true,
-  };
+    fallback: true
+  }
 }
 
 // STEP 2: tell next.js how to get data for each individual recipe
@@ -50,8 +50,9 @@ export async function getStaticProps({ params }) {
     `*[_type == "recipe" && slug.current == $slug][0]{
       name,
       slug
-    }`, { slug }
-  );
+    }`,
+    { slug }
+  )
 
-  return { props: { recipe } };
+  return { props: { recipe } }
 }
