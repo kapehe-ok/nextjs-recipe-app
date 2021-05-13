99c9b435962d5a2e51ba9ca5ccb8841d0e63f913
Kapehe <sarahkapehe@gmail.com>
2021-05-13 13:31:13 -0700

    index.js updates

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
 import Head from "next/head";
 import Link from "next/link";
-import { sanityClient } from "../lib/sanity";
-import imageUrlBuilder from "@sanity/image-url";
+import { sanityClient, urlFor } from "../lib/sanity";
 
-function urlFor(source) {
-  return imageUrlBuilder(sanityClient).image(source);
-}
+const recipesQuery = `*[_type == "recipe" && slug.current != null]{
+  _id,
+  name,
+  slug,
+  mainImage{
+    asset->{
+      _id,
+      url
+    }
+  },
+}`;
 
 export default function Home({ recipes }) {
+  console.log(recipes);
   return (
     <div>
       <Head>
@@ -17,39 +25,24 @@ export default function Home({ recipes }) {
 
       <h1>Welcome to Kaps's Kitchen</h1>
 
-      {/* show all recipes */}
       <ul className="recipes-list">
-        {recipes.map((recipe) => (
-          <li key={recipe._id} className="recipe-card">
-            <Link href={`/recipes/${recipe.slug.current}`}>
-              <a>
-                <img src={urlFor(recipe.mainImage).url()} />
-                <span>{recipe.name}</span>
-              </a>
-            </Link>
-          </li>
-        ))}
+        {recipes.length > 0 &&
+          recipes.map((recipe) => (
+            <li key={recipe._id} className="recipe-card">
+              <Link href={`/recipes/${recipe.slug.current}`}>
+                <a>
+                  <img src={urlFor(recipe.mainImage).url()} />
+                  <span>{recipe.name}</span>
+                </a>
+              </Link>
+            </li>
+          ))}
       </ul>
     </div>
   );
 }
 
 export async function getStaticProps() {
-  // get props from sanity using groq
-  // DO IT
-  const recipes = await sanityClient.fetch(
-    `*[_type == "recipe"]{
-      _id,
-      name,
-      slug,
-      mainImage{
-        asset->{
-          _id,
-          url
-        }
-      },
-    }`
-  );
-
+  const recipes = await sanityClient.fetch(recipesQuery);
   return { props: { recipes } };
 }
